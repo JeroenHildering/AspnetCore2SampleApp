41e9991bf7e7864e0f9cf225bbb9efcb51073c29
Jeroen Hildering <j.hildering@live.nl>
2017-12-06 08:32:26 +0100

    Created .NET Core 2 Sample App

---
+++ test/SampleApp.Api.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleApp.Api.Controllers;
+using Xunit;
+
+namespace SampleApp.Api.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly HomeController _controller;
+
+        public HomeControllerTests()
+        {
+            _controller = new HomeController();
+        }
+
+        [Fact]
+        public void TestGet()
+        {
+            var actionResult = _controller.Get();
+
+            var objectResult = Assert.IsAssignableFrom<OkObjectResult>(actionResult);
+            var value = Assert.IsAssignableFrom<string>(objectResult.Value);
+            Assert.Equal("Hello World!", value);
+        }
+    }
+}
